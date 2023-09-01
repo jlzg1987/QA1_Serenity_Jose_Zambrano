63b5b54349ce9ca166be806a3ab378dbaef1fc11
jlzg1987 <jlzg1987@hotmail.com>
2023-09-01 12:11:47 -0500

    update

--- src/main/java/QA1/userinterfaces/ShowPageInterface.java
+++ src/main/java/QA1/userinterfaces/ShowPageInterface.java
@@ -11,8 +11,8 @@ public class ShowPageInterface {
     public  static final Target LINK_ARTICULO2= Target.the( "Link Articulo encontrado").located(By.id("card-title"));
     public  static final Target BOTON_AGREGAR2= Target.the( "Boton agregar al carrito").locatedBy("//div[@class='col-sm-12 col-md-6 col-lg-6']");
     public  static final Target BOTON_ALERT2= Target.the( "Boton aceptar articulo").locatedBy("alert.accept()");
-    public  static final Target BOTON_ABRIR_CARRITO= Target.the( "Boton abrir carrito").located(By.id("cartur']"));
-    public  static final Target BOTON_COMPRAR= Target.the( "Boton comprar").locatedBy("//button[@Class=\"btn btn-success\"]']");
+    public  static final Target BOTON_ABRIR_CARRITO= Target.the( "Boton abrir carrito").locatedBy("//*[@onclick='showcart()']");
+    public  static final Target BOTON_COMPRAR= Target.the( "Boton comprar").locatedBy("//button[@Class='btn btn-success']");
 
 
 }
